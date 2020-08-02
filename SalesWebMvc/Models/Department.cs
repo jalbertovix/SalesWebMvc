fff0c8dcb916032d0f3e767cbd88c7ff32d79c2d
jalbertovix <joaoalberto2021@gmail.com>
2020-08-02 19:41:48 -0300

    Other entities and second migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace SalesWebMvc.Models
 {
@@ -9,5 +8,26 @@ namespace SalesWebMvc.Models
    {
       public int Id { get; set; }
       public string Name { get; set; }
+      public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+      public Department()        // Esse construtor vazio só éÉ obrigatório porque também 
+      {                          // existirá um construtor com argumentos
+      }
+
+      public Department(int id, string name)
+      {
+         Id = id;                // Não devem ser incluídos no construtor as coleções. Dessa forma,
+         Name = name;            // a coleção Sellers não fará parte desse construtor
+      }
+
+      public void AddSeller(Seller seller)
+      {
+         Sellers.Add(seller);
+      }
+
+      public double TotalSales(DateTime initial, DateTime final)
+      {
+         return Sellers.Sum(seller => seller.TotalSales(initial, final));
+      }
    }
 }
