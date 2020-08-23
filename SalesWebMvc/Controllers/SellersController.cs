29e26622ad394a1d416af87dbc1a52eacc7de510
jalbertovix <joaoalberto2021@gmail.com>
2020-08-22 21:37:20 -0300

    Simple Create form

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
 using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
@@ -20,6 +21,19 @@ namespace SalesWebMvc.Controllers
       {
          var list = _sellerService.FindAll();         // que acessa o Model e
          return View(list);                           // encaminha a lista resultante para a View
+      }
+
+      public IActionResult Create()
+      {
+         return View();
+      }
+
+      [HttpPost]
+      [ValidateAntiForgeryToken]
+      public IActionResult Create(Seller seller)
+      {
+         _sellerService.Insert(seller);
+         return RedirectToAction(nameof(Index));
       }
    }
 }
